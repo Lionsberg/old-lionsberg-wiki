7d08a1f2069e39ead3d4e422de61388998569981
Peter Kaminski <kaminski@istori.com>
2022-05-27 17:31:00 -0700

    automated commit

--- backup.sh
+++ backup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-WORKDIR="/Users/peterkaminski/Documents/Syncthing/Meta Project Sandbox"
+# DO NOT RUN THIS DIRECTLY FROM THE REPO AS IT MAY BE CHANGED BY OTHER
+# PEOPLE. Copy to another location and ensure it's safe to run.
+
+# To run on macOS, you may need to grant `/usr/sbin/cron` access in
+# `Security & Privacy > Privacy > Full Disk Access`, to allow it to
+# run from the directory you have it in, and to allow git the access
+# it needs. Use Cmd+Shift+G to directly type the folder location.
+
+# Crontab line example:
+# 11 * * * * /usr/local/bin/meta-project-sandbox-backup.sh
+
+WORKDIR="/path/to/repo/goes/here"
 
 git -C "$WORKDIR" add .
 git -C "$WORKDIR" commit -m "automated commit"
